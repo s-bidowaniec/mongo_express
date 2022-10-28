e551c738c18307921dbecfd8304d9e326b346e64
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-10-28 14:07:45 +0200

    Add department and employee model tests

---
+++ models/tests/department.test.js
@@ -0,0 +1,46 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+    // ERROR - lack of required parameters
+    it('should throw an error if no "name" arg', () => {
+        const dep = new Department({}); // create new Department, but don't set `name` attr value
+        dep.validate(err => {
+            expect(err.errors.name).to.exist;
+        });
+    });
+    // ERROR - argument type
+    it('should throw an error if "name" is not a string', () => {
+        const cases = [{}, []];
+        for(let name of cases) {
+            const dep = new Department({ name });
+            dep.validate(err => {
+                expect(err.errors.name).to.exist;
+            });
+        }
+    });
+    // ERROR - argument length
+    it('should throw an error if "name" lengths exceeds range 5-20', () => {
+        const cases = ['', 'a', 'aaaa', 'qwertyqwertyqwertyqwe'];
+        for(let name of cases) {
+            const dep = new Department({ name });
+            dep.validate(err => {
+                expect(err.errors.name).to.exist;
+            });
+        }
+    });
+    // VALID
+    it('should not throw an error if "name" is ok', () => {
+        const cases = ['qwert', 'department_name', 'qwertyqwertyqwertyqw'];
+        for(let name of cases) {
+            const dep = new Department({ name });
+            dep.validate(err => {
+                expect(err).to.be.null;
+            });
+        }
+    });
+    after(() => {
+        mongoose.models = {};
+    });
+});
---
+++ models/tests/employee.test.js
@@ -0,0 +1,65 @@
+const Employee = require('../employee.model')
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+const Department = require("../department.model");
+
+
+describe('Employee', () => {
+    // ERROR - lack of required parameters
+    it('should throw an error if no "firstName" arg', () => {
+        const emp = new Employee({lastName: "Surname", departmentID: "depId"}); // create new Employee, but don't set `firstName` attr value
+        emp.validate(err => {
+            expect(err.errors.firstName).to.exist;
+        });
+    });
+    it('should throw an error if no "lastName" arg', () => {
+        const emp = new Employee({firstName: "Name", departmentID: "depId"}); // create new Employee, but don't set `lastName` attr value
+        emp.validate(err => {
+            expect(err.errors.lastName).to.exist;
+        });
+    });
+    it('should throw an error if no "departmentID" arg', () => {
+        const emp = new Employee({firstName: "Name", lastName: "Surname"}); // create new Employee, but don't set `departmentID` attr value
+        emp.validate(err => {
+            expect(err.errors.departmentID).to.exist;
+        });
+    });
+    // ERROR - argument type
+    it('should throw an error if "firstName" is not a String', () => {
+        const cases = [[], {}, undefined];
+        for(let caseValue of cases) {
+            const emp = new Employee({firstName: caseValue, lastName: "Surname", departmentID: "depId"});
+            emp.validate(err => {
+                expect(err.errors.firstName).to.exist;
+            });
+        }
+    });
+    it('should throw an error if "lastName" is not a String', () => {
+        const cases = [[], {}, undefined];
+        for(let caseValue of cases) {
+            const emp = new Employee({firstName: "Name", lastName: caseValue, departmentID: "depId"});
+            emp.validate(err => {
+                expect(err.errors.lastName).to.exist;
+            });
+        }
+    });
+    it('should throw an error if "departmentID" is not a String', () => {
+        const cases = [[], {}, undefined];
+        for(let caseValue of cases) {
+            const emp = new Employee({firstName: "Name", lastName: "Surname", departmentID: caseValue});
+            emp.validate(err => {
+                expect(err.errors.departmentID).to.exist;
+            });
+        }
+    });
+    // VALID
+    it('should not throw an error with proper input data', () => {
+        const emp = new Employee({firstName: "Name", lastName: "Surname", departmentID: "depId"}); // create new Employee, but don't set `firstName` attr value
+        emp.validate(err => {
+            expect(err).to.be.null;
+        });
+    });
+    after(() => {
+        mongoose.models = {};
+    });
+});
